d6f367e73d8948794225e9f7c0eb70661cca9810
Jiwondev <sylvester7412@gmail.com>
2023-12-31 23:31:21 +0900

    init/no-jira: 프로젝트 초기화

---
+++ src/main/java/com/Horang/HorangApplication.java
@@ -0,0 +1,13 @@
+package com.Horang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Horang/HorangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Horang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
